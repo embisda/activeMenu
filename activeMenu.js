a0445d91aeb18617c078e66acc7815828cb4c921
EMBISDA <usfame@gmail.com>
2017-03-20 21:02:50 +0300

    Update activeMenu.js
    Готовый скрипт

--- activeMenu.js
+++ activeMenu.js
@@ -1,15 +1,11 @@
-//ѕолучаем URL текущей страницы относительно корн€
 var x = window.location.pathname;
-
-//ѕолучаем массив элементов с классом punkt-menu
-var punktMenu = document.getElementsByClassName('punkt-menu');
-
-//ѕроходим циклом по всем пунктам меню и 
-//убираем атрибут href у текущей страницы
-for (var i = 0; i<punktMenu.length; i++) {
-	z = "/I:/Web/" + punktMenu[i].getAttribute('href');
+		
+var navMenu = document.getElementsByClassName('navMenu');
+		
+for (var i = 0; i<navMenu.length; i++) {
+	var z = navMenu[i].getAttribute('href');
 	if (x == z) {
-		punktMenu[i].removeAttribute('href');
-		punktMenu[i].className = "punkt-menu-active";
+		navMenu[i].removeAttribute('href');
+		navMenu[i].className = "navMenu-active";
 	}	
 }		
